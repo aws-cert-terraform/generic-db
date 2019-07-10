c22c874f7eca0c76e7ad6281dd4c2ab59c72fc56
Ian Cullinane <ian.cullinane@gmail.com>
2019-07-10 10:24:40 -0400

    add broken sg list

--- generic-db.tf
+++ generic-db.tf
@@ -1,7 +1,7 @@
 
 
 resource "aws_db_instance" "default" {
-  allocated_storage      = ${var.size}
+  allocated_storage      = "${var.size}"
   storage_type           = "${var.storage_type}"
   engine                 = "${var.engine}"
   engine_version         = "${var.engine_version}"
