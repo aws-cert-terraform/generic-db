644d71996866294e812cfac0db19129fb8185334
Ian Cullinane <ian.cullinane@gmail.com>
2019-07-26 14:23:18 -0400

    add service account lookup to policy

--- generic-db.tf
+++ generic-db.tf
@@ -40,4 +40,5 @@ resource "aws_security_group_rule" "allow_mysql_port" {
   to_port = 3306
   protocol = "tcp"
   source_security_group_id = var.source_sg
+  security_group_id = element(var.security_group_ids, 0)
 }
